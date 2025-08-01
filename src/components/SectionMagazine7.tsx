ac619dfd3fd815c2a6d450a707ec6d8b4ee02f13
rufaid-safcodes <rufaid@safcodes.com>
2025-08-01 17:53:45 +0530

    holy quran customize

--- src/components/SectionMagazine7.tsx
+++ src/components/SectionMagazine7.tsx
@@ -2,8 +2,8 @@ import { TPost } from '@/data/posts'
 import HeadingWithSub, { HeadingWithSubProps } from '@/shared/Heading'
 import clsx from 'clsx'
 import { FC } from 'react'
-import Card10 from './PostCards/Card10'
 import Card10V3 from './PostCards/Card10V3'
+import Card17Podcast from './PostCards/Card17Podcast'
 
 type Props = Pick<HeadingWithSubProps, 'subHeading' | 'dimHeading'> & {
   posts: TPost[]
@@ -22,11 +22,14 @@ const SectionMagazine7: FC<Props> = ({ posts, className, heading, subHeading, di
           <Card10V3 post={posts[0]} />
           <Card10V3 galleryType={2} post={posts[1]} />
         </div>
-        <div className="mt-3 grid grid-cols-1 gap-6 sm:grid-cols-2 md:gap-8 lg:grid-cols-3 xl:grid-cols-4">
-          <Card10 post={posts[2]} />
+        <div className="mt-3 grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-7 lg:grid-cols-3">
+          {/* <Card10 post={posts[2]} />
           <Card10 post={posts[3]} />
           {posts[4] && <Card10 post={posts[4]} />}
-          {posts[5] && <Card10 post={posts[5]} />}
+          {posts[5] && <Card10 post={posts[5]} />} */}
+          {posts.slice(0, 6).map((p) => (
+            <Card17Podcast key={p.id} post={p} />
+          ))}
         </div>
       </div>
     </div>
