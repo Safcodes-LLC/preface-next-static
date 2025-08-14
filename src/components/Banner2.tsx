96809d36e98394478d341722923e30e06dcc2ff7
rufaid-safcodes <rufaid@safcodes.com>
2025-08-14 09:31:31 +0530

    banner update

--- src/components/Banner2.tsx
+++ src/components/Banner2.tsx
@@ -14,13 +14,13 @@ const Banner2 = ({ image, title, alt, className = '' }: Banner2Props) => {
         alt={alt || title}
         fill
         src={image}
-        className="object-cover md:rounded-2xl"
+        className="object-cover"
         sizes="(max-width: 1600px) 100vw, 95vw"
         priority
       />
       {/* Linear gradient overlay - left to right fade */}
       <div
-        className="absolute inset-y-0 left-0 w-3/5 md:rounded-l-2xl"
+        className="absolute inset-y-0 left-0 w-3/5 "
         style={{
           background: 'linear-gradient(90deg, rgba(20, 20, 20, 0.76) 0%, rgba(97, 97, 97, 0) 100%)',
         }}
