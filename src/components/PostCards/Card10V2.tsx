ada7cd7c8862b2bda685a6f1e6b1467c4724cd9e
rufaid-safcodes <rufaid@safcodes.com>
2025-09-04 21:21:37 +0530

    add link and comment console

--- src/components/PostCards/Card10V2.tsx
+++ src/components/PostCards/Card10V2.tsx
@@ -15,7 +15,6 @@ interface Props {
 
 const Card10V2: FC<Props> = ({ className, post, ratio = 'aspect-square sm:aspect-11/12' }) => {
   const { handle, categories, bookmarked } = post
-  // console.log(post,"post check for islam fro beginners");
   
   const [isHover, setIsHover] = useState(false)
 
