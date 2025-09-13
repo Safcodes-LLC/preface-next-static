5710bd61685491b08e5d2730c463f3d993803a59
rufaid-safcodes <rufaid@safcodes.com>
2025-09-13 10:12:10 +0530

    remove unused code

--- src/app/[lang]/stories/layout.tsx
+++ src/app/[lang]/stories/layout.tsx
@@ -1,12 +1,5 @@
 import { ReactNode } from 'react'
 import { ApplicationLayout } from '@/app/[lang]/application-layout'
-import { getAuthors } from '@/data/authors'
-import { getCategories } from '@/data/categories'
-// import BackgroundSection from '@/components/BackgroundSection'
-// import SectionGridCategoryBox from '@/components/SectionGridCategoryBox'
-// import SectionSliderNewAuthors from '@/components/SectionSliderNewAuthors'
-// import SectionSubscribe2 from '@/components/SectionSubscribe2'
-// import ButtonSecondary from '@/shared/ButtonSecondary'
 
 interface Props {
   children: ReactNode
@@ -14,8 +7,6 @@ interface Props {
 }
 
 const Layout: React.FC<Props> = async ({ children, params }) => {
-  const categories = await getCategories()
-  const authors = await getAuthors()
 
   return (
     <ApplicationLayout params={params}>
