69d0d49ecf3ffd75da8908085361a493019c8181
akmmubashir <akmmubashir@gmail.com>
2025-09-15 16:34:15 +0400

    stories page loader and skelton added, also dictionary and prettier enabled

--- src/components/PostCards/Card11.tsx
+++ src/components/PostCards/Card11.tsx
@@ -1,11 +1,10 @@
 'use client'
-
 import { TPost } from '@/data/posts'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { FC, useState } from 'react'
-import PostFeaturedMedia from '../PostFeaturedMedia/PostFeaturedMedia'
+// import PostFeaturedMedia from '../PostFeaturedMedia/PostFeaturedMedia'
 import Image from 'next/image'
 
 interface Props {
