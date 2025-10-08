75ac4a41d8db927f45cc546b430cac5f96fc449c
rufaid-safcodes <rufaid@safcodes.com>
2025-10-08 11:42:38 +0530

    arrow hide in storytelling islam

--- src/components/CategoryCards/CardCategory3.tsx
+++ src/components/CategoryCards/CardCategory3.tsx
@@ -1,5 +1,4 @@
 import { TCategory } from '@/data/categories'
-import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import Link from 'next/link'
 import { FC } from 'react'
@@ -30,14 +29,14 @@ const CardCategory3: FC<Props> = ({ className = '', category, lang }) => {
         <span className="absolute inset-0 bg-black/30 opacity-0 transition-opacity group-hover:opacity-100"></span>
 
         {/* Arrow overlay */}
-        <div
+        {/* <div
           className="absolute inset-0 flex items-center justify-center opacity-0 transition-all duration-300 group-hover:opacity-100"
           onClick={(e) => e.stopPropagation()}
         >
           <div className="transform rounded-full bg-white/90 p-3 shadow-lg backdrop-blur-sm transition-all duration-300 hover:scale-110 hover:bg-white dark:bg-gray-800/90 dark:hover:bg-gray-800">
             <ArrowRightIcon className="h-5 w-5 text-gray-700 rtl:rotate-180 dark:text-gray-200" />
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="mt-4 text-center">
         <h2 className={`line-clamp-2 text-lg font-medium text-neutral-900 dark:text-neutral-100`}>{name}</h2>
