eebe4458fcdea906520b7644994a00fcde403230
rufaid-safcodes <rufaid@safcodes.com>
2025-10-29 10:16:09 +0530

    optional chaining set

--- src/components/SectionMagazine12.tsx
+++ src/components/SectionMagazine12.tsx
@@ -17,44 +17,44 @@ const SectionMagazine12: FC<Props> = async ({ className, lang, dict }) => {
 
   return (
     <div className={clsx('section-magazine-12 relative', className)}>
-      {customFeatured.data.length > 0 && customFeatured.data[0].status === 'Active' ? (
+      {customFeatured?.data?.length > 0 && customFeatured?.data[0].status === 'Active' ? (
         <Card21
-          post={customFeatured.data[0]}
+          post={customFeatured?.data[0]}
           dict={dict}
           content={{
-            id: customFeatured.data[0].id,
-            link: customFeatured.data[0].link,
-            title: customFeatured.data[0].title,
-            content: customFeatured.data[0].content,
-            image: customFeatured.data[0].featured_image,
+            id: customFeatured?.data[0]?._id,
+            link: customFeatured?.data[0]?.link,
+            title: customFeatured?.data[0]?.title,
+            content: customFeatured?.data[0]?.content,
+            image: customFeatured?.data[0]?.featured_image,
             likeAndBookmark: false,
           }}
           lang={lang}
         />
-      ) : highlightedFeatured.data.length > 0 ? (
+      ) : highlightedFeatured?.data?.length > 0 ? (
         <Card21
-          post={highlightedFeatured.data[0]}
+          post={highlightedFeatured?.data[0]}
           dict={dict}
           content={{
-            id: highlightedFeatured.data[0]._id,
-            link: highlightedFeatured.data[0].slug,
-            title: highlightedFeatured.data[0].title,
-            content: highlightedFeatured.data[0].content,
-            image: highlightedFeatured.data[0].featured_image,
+            id: highlightedFeatured?.data[0]?._id,
+            link: highlightedFeatured?.data[0]?.slug,
+            title: highlightedFeatured?.data[0]?.title,
+            content: highlightedFeatured?.data[0]?.content,
+            image: highlightedFeatured?.data[0]?.featured_image,
             likeAndBookmark: true,
           }}
           lang={lang}
         />
-      ) : randomFeatured.data.length > 0 ? (
+      ) : randomFeatured?.data?.length > 0 ? (
         <Card21
-          post={randomFeatured.data[0]}
+          post={randomFeatured?.data[0]}
           dict={dict}
           content={{
-            id: randomFeatured.data[0]._id,
-            link: randomFeatured.data[0].slug,
-            title: randomFeatured.data[0].title,
-            content: randomFeatured.data[0].content,
-            image: randomFeatured.data[0].featured_image,
+            id: randomFeatured?.data[0]?._id,
+            link: randomFeatured?.data[0]?.slug,
+            title: randomFeatured?.data[0]?.title,
+            content: randomFeatured?.data[0]?.content,
+            image: randomFeatured?.data[0]?.featured_image,
             likeAndBookmark: true,
           }}
           lang={lang}
