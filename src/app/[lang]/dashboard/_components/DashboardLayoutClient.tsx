cfe9bd1ec98e6cf54b1d8c166d8ec46373cacd8a
rufaid-safcodes <rufaid@safcodes.com>
2025-11-13 16:26:19 +0530

    cursor pointer

--- src/app/[lang]/dashboard/_components/DashboardLayoutClient.tsx
+++ src/app/[lang]/dashboard/_components/DashboardLayoutClient.tsx
@@ -31,7 +31,7 @@ const user = {
   name: 'John Doe',
   email: 'john@example.com',
   imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    '/images/Preface-Logo.png',
 }
 
 const navigation = [
@@ -129,7 +129,7 @@ export default function DashboardLayoutClient({ children }: { children: React.Re
                   <Avatar
                     alt="avatar"
                     src={profile?.profile_pic || user.imageUrl}
-                    className="size-8"
+                    className="size-8 cursor-pointer"
                     width={32}
                     height={32}
                     sizes="32px"
